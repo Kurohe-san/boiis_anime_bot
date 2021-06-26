b7652f547c1b15cfcec04ca9ada273925846a23e
Kurohe-san <77454729+Kurohe-san@users.noreply.github.com>
2021-06-26 14:40:22 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='~',intents=intents)
+bot = commands.Bot(command_prefix='!',intents=intents)
 
 
 @bot.event
@@ -29,30 +29,30 @@ async def search(ctx, c: str, s: str):
     try:
         c = int(c)
     except ValueError:
-        ctx.send("Fehler: `Syntaxfehler`\n\nBitte Benutze den Command folgendermaßen: ~search <Anzahl der Elemente> <Name>'")
+        ctx.send("Fehler: `Syntaxfehler`\n\nBitte Benutze den Command folgendermaßen: !search <Anzahl der Elemente> <Name>'")
     try:
         s = AnimeSearch(s)
         for i in range(c):
             await ctx.send(f'{i+1}. {s.results[i].title} [{s.results[i].mal_id}]')
     except Exception:
-        await ctx.send('Fehler: `invalide Animesuche` (Name des Animes muss >3 Zeichen sein) oder Syntaxfehler\n\nBitte Benutze den Command folgendermaßen: ~search <Anzahl der Elemente> <Name>')
+        await ctx.send('Fehler: `invalide Animesuche` (Name des Animes muss >3 Zeichen sein) oder Syntaxfehler\n\nBitte Benutze den Command folgendermaßen: !search <Anzahl der Elemente> <Name>')
         return
-    await ctx.send("Benutze \"~info <id>\" um Informationen zu einem Anime auszugeben")
+    await ctx.send("Benutze \"!info <id>\" um Informationen zu einem Anime auszugeben")
 
 @bot.command(name='sp',help='Ausgabe von _n_ Suchergebnissen zu Anime _m_ per Namen im Privatchat')
 async def sp(ctx, c: str, s: str):
     try:
         c = int(c)
     except ValueError:
-        ctx.send("Fehler: `Syntaxfehler`\n\nBitte Benutze den Command folgendermaßen: ~search <Anzahl der Elemente> <Name>'")
+        ctx.send("Fehler: `Syntaxfehler`\n\nBitte Benutze den Command folgendermaßen: !search <Anzahl der Elemente> <Name>'")
     try:
         s = AnimeSearch(s)
         for i in range(c):
             await ctx.author.send(f'{i+1}. {s.results[i].title} [{s.results[i].mal_id}]')
     except Exception:
-        await ctx.author.send('Fehler: `invalide Animesuche` (Name des Animes muss >3 Zeichen sein) oder Syntaxfehler\n\nBitte Benutze den Command folgendermaßen: ~search <Anzahl der Elemente> <Name>')
+        await ctx.author.send('Fehler: `invalide Animesuche` (Name des Animes muss >3 Zeichen sein) oder Syntaxfehler\n\nBitte Benutze den Command folgendermaßen: !search <Anzahl der Elemente> <Name>')
         return
-    await ctx.author.send("Benutze \"~info <id>\" um Informationen zu einem Anime auszugeben")
+    await ctx.author.send("Benutze \"!info <id>\" um Informationen zu einem Anime auszugeben")
 
 @bot.command(name='info',help='Ausgabe von Informationen zu Anime per Anime-id')
 async def info(ctx, id: int):
@@ -97,6 +97,25 @@ async def watch(ctx,id: str):
     a = Anime(id)
     await ctx.send(f"Du bist erfolgreich der Watchparty für `{a.title}` beigetreten.")
 
+# Leave watchparty via id
+@bot.command(name='leave',help='Entferne dich aus einer Watchparty eines Animes per Anime-id')
+async def leave(ctx,id: str):
+    with open("userconfig.json",'r+') as f:
+        j = json.load(f)
+        try:
+            if ctx.author.id in j[str(id)]:
+                j[str(id)].remove(ctx.author.id)
+            else:
+                await ctx.send("Fehler: kein Mitglied der Watchparty")
+                return
+        except KeyError:
+            j[str(id)] = [ctx.author.id]
+        f.seek(0)
+        json.dump(j,f)
+    # Frontend message
+    a = Anime(id)
+    await ctx.send(f"Du hast die Watchparty für `{a.title}` verlassen.")
+
 # Join Watchparty via AnimeSearch
 @bot.command(name='ws',help='Füge dich zu einer Watchparty eines Animes per Anime-Suche hinzu')
 async def ws(ctx,s: str):
@@ -125,6 +144,37 @@ async def ws(ctx,s: str):
             # Frontend message
         a = Anime(id)
         await ctx.send(f"Du bist erfolgreich der Watchparty für `{a.title}` beigetreten.")
+    except Exception:
+        await ctx.send("Fehler: `invalide Animesuche` (Name des Animes muss >3 Zeichen sein)")
+
+# Leave Watchparty via AnimeSearch
+@bot.command(name='ls',help='Entferne dich aus einer Watchparty eines Animes per Anime-Suche')
+async def ls(ctx,s: str):
+    ans = ""
+    try:
+        s = AnimeSearch(s)
+        for i in range(10):
+            await ctx.author.send(f'{i+1}. {s.results[i].title} [{s.results[i].mal_id}]')
+        def check(m):
+            return m.channel == ctx.author.dm_channel
+        ans = await bot.wait_for("message", check=check)
+        id = s.results[int(ans.content)-1].mal_id
+        with open("userconfig.json",'r+') as f:
+            
+            j = json.load(f)
+            try:
+                if not ctx.author.id in j[str(id)]:
+                    j[str(id)].append(ctx.author.id)
+                else:
+                    await ctx.author.send("Fehler: kein Mitglied der Watchparty")
+                    return
+            except KeyError:
+                j[str(id)] = [ctx.author.id]
+            f.seek(0)
+            json.dump(j,f)
+            # Frontend message
+        a = Anime(id)
+        await ctx.send(f"Du bist die Watchparty für `{a.title}` verlassen.")
     except Exception:
         await ctx.send("Fehler: `invalide Animesuche` (Name des Animes muss >3 Zeichen sein)")
 
@@ -181,5 +231,11 @@ async def list(ctx,id: int):
         else:
             await ctx.send(f"{Anime(id).title} hat noch keine Mitglieder!")
 
-
+@bot.command(name='clear',help='Alle Nachrichten des Bots und Commands an den Bot im aktuellen Textchannel löschen')
+async def clear(ctx,n: int = 100):
+    async for message in ctx.channel.history(limit=n):
+        if message.author == bot.user:
+            await message.delete()
+        elif message.content.startswith("!") or message.content.startswith("~"):
+            await message.delete()
 bot.run(TOKEN)
